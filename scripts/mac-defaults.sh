f870e13f71c1bb29ec02bcf816176f7cd004bdea
Jordan Foreman <hello@jordanforeman.com>
2024-03-01 14:24:09 -0600

    fix: only install casks when --with-cask is provided to the install.sh script

--- scripts/mac-defaults.sh
+++ scripts/mac-defaults.sh
@@ -1,31 +1,48 @@
 #!/bin/zsh
 
+WITH_CASK=""
+# Parse command line arguments
+for arg in "$@"
+do
+    case $arg in
+        --with-cask)
+        WITH_CASK="--with-cask"
+        shift # Remove --with-cask from processing
+        ;;
+        *)
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
 # Check if the current OS is macOS
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo "Detected macOS"
+    if [[ "$WITH_CASK" == "--with-cask" ]]; then
+        echo "Installing Homebrew Casks..."
 
-    # Check if Homebrew is installed
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew not found. Installing..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        # Check if Homebrew is installed
+        if ! command -v brew &> /dev/null; then
+            echo "Homebrew not found. Installing..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+
+        # Brew > Cask
+        brew install --cask \
+            visual-studio-code \
+            iterm2 \
+            dbeaver-community \
+            fantastical \
+            todoist \
+            obsidian \
+            1password \
+            altserver \
+            discord \
+            brave-browser \
+            protonvpn \
+            vlc \
+            zoom \
+            intellij-idea
+    else
+        echo "--with-cask option is intended for macOS only."
     fi
-
-    # Brew > Cask
-    brew install --cask \
-        visual-studio-code \
-        iterm2 \
-        dbeaver-community \
-        fantastical \
-        todoist \
-        obsidian \
-        1password \
-        altserver \
-        discord \
-        brave-browser \
-        protonvpn \
-        vlc \
-        zoom \
-        intellij-idea
-else
-    echo "This script is intended for macOS only."
 fi
