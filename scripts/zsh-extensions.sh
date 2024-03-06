f845321c81fbcdbc9f2127af77abbcd36e9c3ceb
Jordan Foreman <hello@jordanforeman.com>
2024-03-05 19:25:01 -0600

    chore: add zsh-autosuggestions

--- scripts/zsh-extensions.sh
+++ scripts/zsh-extensions.sh
@@ -4,3 +4,8 @@ if [ ! -d "./ext/zsh-autocomplete" ] ; then
   git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ./ext/zsh-autocomplete
 fi
 write_to_file "source "$(pwd)"/ext/zsh-autocomplete/zsh-autocomplete.plugin.zsh" ~/.profile
+
+if [ ! -d "./ext/zsh-autocomplete" ] ; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+write_to_file "source $(pwd)/ext/zsh-autosuggestions/zsh-autosuggestions.zsh" ~/.profile
