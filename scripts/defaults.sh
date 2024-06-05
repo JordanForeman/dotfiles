41e6524a7d48d83e8984e7af9efd879eaf063f5b
Jordan Foreman <thejordanforeman@gmail.com>
2024-06-04 20:45:55 -0500

    feat: install bun

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -1,2 +1,20 @@
 echo "Installing Oh My ZSH!"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Check if Bun is installed
+if ! command -v bun &> /dev/null; then
+    echo "Bun is not installed. Installing..."
+
+    # Install Bun
+    curl -fsSL https://bun.sh/install | bash
+
+    # Check if Bun was installed successfully
+    if command -v bun &> /dev/null; then
+        echo "Bun was installed successfully!"
+    else
+        echo "Failed to install Bun."
+        exit 1
+    fi
+else
+    echo "Bun is already installed."
+fi
