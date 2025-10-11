cb858993b968d3b60ec7a8947e96def2855a9dfe
Jordan Foreman <hello@jordanforeman.com>
2025-10-11 14:17:30 -0500

    Add keymaps for Ctrl-d and Ctrl-u
    Add custom keymaps for scrolling in normal mode.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-d>", function()
+  vim.cmd([[normal! <C-d>zzz.]])
+end)
+
+vim.keymap.set("n", "<C-u>", function()
+  vim.cmd([[normal! <C-u>zzz.]])
+end)
