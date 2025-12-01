396b84877f8c2988bcabaa069053fe6079dd0a55
Jordan Foreman <hello@jordanforeman.com>
2025-11-30 21:07:22 -0600

    Update sketchybar config

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -5,13 +5,9 @@ FOCUSED=$(aerospace list-workspaces --focused)
 if [ "$1" = "$FOCUSED" ]; then
   sketchybar --set "$NAME" \
     icon="●" \
-    icon.color=0xff89b4fa \
-    background.color=0xff45475a \
-    background.border_color=0xff89b4fa
+    icon.color=0xff89b4fa
 else
   sketchybar --set "$NAME" \
     icon="$1" \
-    icon.color=0xffcdd6f4 \
-    background.color=0xff313244 \
-    background.border_color=0xff45475a
+    icon.color=0xffcdd6f4
 fi
