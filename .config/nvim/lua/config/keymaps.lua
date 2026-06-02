15b4fec8b265c8ae5aa11ebde1550707e425ead8
Jordan Foreman <hello@jordanforeman.com>
2026-06-01 21:32:57 -0500

    fix: restore nvim page scroll keymaps
    Replace the Lua callback mappings for Ctrl+u and Ctrl+d with direct normal-mode mappings so Neovim interprets the page scroll commands correctly. Keep the intended centering and first-non-whitespace cursor positioning behavior.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-d>", function()
-  vim.cmd([[normal! <C-d>zzz.]])
-end)
-
-vim.keymap.set("n", "<C-u>", function()
-  vim.cmd([[normal! <C-u>zzz.]])
-end)
+vim.keymap.set("n", "<C-d>", "<C-d>zz_", { desc = "Page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz_", { desc = "Page up and center" })
 
 vim.keymap.set("n", "<leader>gO", ":!gh browse %:.<CR>", { desc = "Open Current File in GitHub" })
